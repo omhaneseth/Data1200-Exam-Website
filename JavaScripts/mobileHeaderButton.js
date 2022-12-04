bd73cce24b912aefb5a206de3f8fd222c2bdd2c9
Ole Martin Haneseth <113453475+omhaneseth@users.noreply.github.com>
2022-12-04 20:47:26 +0100

    La til kommentarer i html koden

--- JavaScripts/mobileHeaderButton.js
+++ JavaScripts/mobileHeaderButton.js
@@ -1,3 +1,6 @@
+
+// Kode som fjerner/legger til en klasse basert på knappetrykk i html
+// Koden er hentet fra W3schools: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 function mobileMenuButton() {
           var x = document.getElementById("theMobileNav");
           if (x.className === "mobileNav") {
